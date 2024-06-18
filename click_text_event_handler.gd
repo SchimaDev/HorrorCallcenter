3e0d927f5e1a92ccf26bd625da06a29e1671af95
Alexander Schimanko <as323@hdm-stuttgart.de>
2024-06-18 12:20:29 +0200

    fixed bug with getting the string from [url] tag

--- click_text_event_handler.gd
+++ click_text_event_handler.gd
@@ -10,5 +10,5 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func handle_url_tag_clicked(name: String) -> void:
-	print(name)
+func handle_url_tag_clicked(meta: Variant) -> void:
+	print(str(meta))
