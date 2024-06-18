05717ce9d6c994c67d7976e47130d1be57d56977
Alexander Schimanko <as323@hdm-stuttgart.de>
2024-06-18 11:11:41 +0200

    added ClickTextEventHandler for future use

---
+++ click_text_event_handler.gd
@@ -0,0 +1,14 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func handle_url_tag_clicked(name: String) -> void:
+	print("potato")
