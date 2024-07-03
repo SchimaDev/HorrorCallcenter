c2571a5c6322dca0edb233ac8f6089e05a459e75
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2024-07-03 19:37:42 +0200

    implemented book page flipping with B and N keys

--- scripts/Encyclopedia.gd
+++ scripts/Encyclopedia.gd
@@ -2,15 +2,29 @@ class_name Encyclopedia
 extends Node3D
 
 @onready var animation_player = $AnimationPlayer
+@export var tex : Array[StandardMaterial3D]
+@onready var cube = $Armature_001/Skeleton3D/Cube_001
+
+var page = -1
 
 func _ready():
 	pass
 
-func _on_rich_text_label_meta_clicked(meta):
-	ClickTextEventHandler.handle_Encyclopedia_url_tag_clicked(meta)
+#func _on_rich_text_label_meta_clicked(meta):
+	#ClickTextEventHandler.handle_Encyclopedia_url_tag_clicked(meta)
 
 func _process(delta):
 	if  Input.is_action_just_pressed("book_test"):
-		animation_player.play("Armature_001Action")
+		if page >= tex.size()-1:
+			page = tex.size()-1
+		else:
+			page += 1
+			cube.set_surface_override_material(1, tex[page])
+			animation_player.play("Armature_001Action")
 	if  Input.is_action_just_pressed("book_test_2"):
-		animation_player.stop(false);
+		if page <= -1:
+			page = -1
+		else:
+			cube.set_surface_override_material(1, tex[page])
+			animation_player.play_backwards("Armature_001Action")
+			page -= 1
