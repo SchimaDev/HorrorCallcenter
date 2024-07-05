c3a27e8b09b4df2bbfd6bc063b1aa88dfae65a0e
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2024-07-05 11:17:18 +0200

    cleaned up book texture switch

--- scripts/Encyclopedia.gd
+++ scripts/Encyclopedia.gd
@@ -19,12 +19,14 @@ func _process(delta):
 			page = tex.size()-1
 		else:
 			page += 1
+			animation_player.stop()
 			cube.set_surface_override_material(1, tex[page])
 			animation_player.play("Armature_001Action")
 	if  Input.is_action_just_pressed("book_test_2"):
 		if page <= -1:
 			page = -1
 		else:
+			animation_player.seek(2,5)
 			cube.set_surface_override_material(1, tex[page])
 			animation_player.play_backwards("Armature_001Action")
 			page -= 1
