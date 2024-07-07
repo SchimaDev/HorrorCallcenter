d3eecec8eaea4065f21e707ac597dbf159caf4ce
Ruslan Novikov <ruslan.novikov@hotmail.de>
2024-07-07 19:01:36 +0200

    fmod integration

--- scripts/Encyclopedia.gd
+++ scripts/Encyclopedia.gd
@@ -4,12 +4,19 @@ extends Node2D
 const outline  = preload("res://art/outline.gdshader")
 @onready var image = $Image
 @onready var page = $Page
-#signal openBook
+#signal openBookSignal
+
+# FMOD events
+var openBook: FmodEvent = null
+var closeBook: FmodEvent = null
+var pageTurn: FmodEvent = null
 
 func _ready():
 	page.visible = false
+	loadSoundEvents()
 
 func _on_area_2d_mouse_entered() -> void:
+	pageTurn.start()
 	image.material.shader = outline
 
 
@@ -20,6 +27,7 @@ func _on_area_2d_mouse_exited() -> void:
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
+			openBook.start()
 			page.visible = true
 			get_viewport().set_input_as_handled()
 
@@ -27,6 +35,11 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 func _on_rich_text_label_meta_clicked(meta):
 	ClickTextEventHandler.handle_Encyclopedia_url_tag_clicked(meta)
 
-
 func _on_button_pressed():
 	page.visible = false
+	closeBook.start()
+
+func loadSoundEvents():
+	openBook = FmodServer.create_event_instance("event:/Glossary/OpenBook")
+	closeBook = FmodServer.create_event_instance("event:/Glossary/CloseBook")
+	pageTurn = FmodServer.create_event_instance("event:/Glossary/PageTurn")
