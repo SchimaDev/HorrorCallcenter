82d8e78c8b8dd776d37aa4bb907faf5593ff4c35
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2024-07-20 12:33:38 +0200

    added all interactable pages

---
+++ scripts/static_camera.gd
@@ -0,0 +1,28 @@
+extends Camera3D
+
+@export var targetCamera: Camera3D
+var deskCameraDefaultRotation: Vector3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	CameraShifter.transition_3d_started.connect(_on_transition_3d_started)
+	CameraShifter.transition_3d_finished.connect(_on_transition_3d_finished)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _input(event: InputEvent) -> void:
+	if get_viewport().get_camera_3d() == self:
+		if event is InputEventMouseButton:
+			CameraShifter.transition_to_requested_camera_3d(self, targetCamera, 1)
+
+func _on_transition_3d_started(from: Camera3D, to: Camera3D, duration: float):
+	if to == self:
+		get_parent().get_node("UI/Crosshair").visible = false
+
+func _on_transition_3d_finished(from: Camera3D, to: Camera3D, duration: float):
+	if to == self:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+
+
