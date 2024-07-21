105d44bcf126d426315d7991d0c15a7320e47f8b
Alexander Schimanko <as323@hdm-stuttgart.de>
2024-07-21 14:34:35 +0200

    added some shaders and implemented subviewport for monitor

---
+++ scenes/office_3d.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var layout := Dialogic.start('test_dialogue')
+	$SubViewport.add_child(layout)
+	Dialogic.History.open_requested.emit()
+	pass
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
