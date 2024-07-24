62b1c53eddec2c92af8b41e6f53856e91bca3c6a
ChaoticPan <elisabeth.kintzel@student.htw-berlin.de>
2024-07-24 13:11:28 +0200

    created call queue
    shortly after one call ends, the next dialog will be queued, and the phone rings again

--- art/3D/telephone model/telephone.gd
+++ art/3D/telephone model/telephone.gd
@@ -1,5 +1,8 @@
 extends Node3D
 
+var incomingCall = ""
+var callQueue = ["Lucia_01", "Jonny_01"]
+
 @onready var shader = $"RootNode/tel fijo".get_surface_override_material(0).next_pass
 var targeted = false: 
 	set(val):
@@ -12,17 +15,26 @@ var targeted = false:
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 	setupRingingPhone()
-	pass
+	incomingCall = callQueue.pop_front()
 
 func setupRingingPhone() -> void:
 	FmodEventMessenger.ringingPhone.set_3d_attributes(self.global_transform)
 	FmodEventMessenger.playRingingPhone()
 
 func _input(event: InputEvent) -> void:
-	if targeted:
+	if targeted && incomingCall != "":
 		if event is InputEventMouseButton:
+			Dialogic.VAR.reset()
 			FmodEventMessenger.stopRingingPhone()
-			var dialog = 'Jonny_01'
-			Dialogic.start(dialog)
-			ClickTextEventHandler.dialog = dialog
+			Dialogic.start(incomingCall)
+			ClickTextEventHandler.dialog = incomingCall
+			incomingCall = ""
+
+func _on_dialogic_signal(argument):
+	if argument == "callEnded":
+		await get_tree().create_timer(5).timeout
+		if callQueue.size() > 0:
+			incomingCall = callQueue.pop_front()
+			setupRingingPhone()
